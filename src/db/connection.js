2ab6496f0d7c19e5e740c9b74f35c889621b7c9a
horsellcommon <crawfordj716@gmail.com>
2022-10-11 11:21:12 +0100

    Setup of app.js file

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient(process.env.MONGO_URI)
+const client = new MongoClient(process.env.MONGO_URI);
 
 const connection = async () => {
-    try {
-        // client gives us access to the mongoDB methods
-        await client.connect()
+  try {
+    // client gives us access to the mongoDB methods
+    await client.connect();
 
-        //db name, creaes indiv collection
-        const db = client.db("Films")
+    //db name, creaes indiv collection
+    const db = client.db("Films");
 
-        //grabs collection to perform CRUD ops
-        return db.collection("Film")
-    } catch (error) {
-        console.log(error)
-    }
-}
+    //grabs collection to perform CRUD ops
+    return db.collection("Film");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //returning two different items so we use object syntax
-module.exports = { client, connection }
+module.exports = { client, connection };
 // we export the client and connection so we can have access to our client and connection outside
 // of this file
